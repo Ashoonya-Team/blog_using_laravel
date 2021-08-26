1149e75f4d1055118ee49fa5337ba02e00e43d74
ChaitanyaSurwase <surwasechaitanya001@gmail.com>
2021-08-26 13:29:45 +0530

    Changes to be committed: 	modified:   app/Http/Controllers/signup_controller.php 	->same as it is 	new file:   resources/views/login.blade.php 	->Login page user interface 	modified:   routes/web.php 	->Routes

--- Blog/app/Http/Controllers/signup_controller.php
+++ Blog/app/Http/Controllers/signup_controller.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\support\Facades\DB;
+
 
 class signup_controller extends Controller
 {
@@ -11,6 +13,7 @@ class signup_controller extends Controller
     {
         return view('signup');
     }
+    // store method holds data entered by the user
     public function store()
     {
         $this->validate(request(), [
@@ -18,11 +21,22 @@ class signup_controller extends Controller
             'email' => 'required|email',
             'password' => 'required'
         ]);
-
+        // In below line data will be stored in database
         $user = User::create(request(['name', 'email', 'password']));
-
-
+        // After data stored in database we are redirected to homepage
         return redirect()->to('/home');
     }
+    public function userValidation(Request $req)
+    {
+
+        $pass = DB::table('users')->where('name', $req->uname)->first();
+
+        if ($pass != null && $pass->password == $req->psw)
+
+            return redirect()->to('/home');
+
+        else
+
+            echo "<h3>Unsuccessfull!!</h3>";
+    }
 }
-// }s
